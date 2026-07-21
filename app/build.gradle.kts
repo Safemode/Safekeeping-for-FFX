5cde75664c8839759c75d6e07ab6dc2309d80c0a
Safemode <zsld0423@gmail.com>
2026-07-21 02:10:49 -0400

    add in sphere grid character colors and link nodes via highlighted lines to make it easier to see what's been activated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.safemode.safekeepingforffx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.6.5"
+        versionCode = 5
+        versionName = "0.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
