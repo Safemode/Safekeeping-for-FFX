ef5da2600c5c4915d84076b38fbb46d4201f0a97
Safemode <zsld0423@gmail.com>
2026-07-22 15:02:28 -0400

    Added long-press options to make the capture progress tracking quicker if you use this section mid-run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.safemode.safekeepingforffx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.7.5"
+        versionCode = 14
+        versionName = "0.7.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
