a7bd78ccfed3910c7d6351a58dfc619535d67e31
Safemode <zsld0423@gmail.com>
2026-09-12 12:34:30 -0400

    Updated Gradle and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.safemode.safekeepingforffx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.8.6.1"
+        versionCode = 26
+        versionName = "0.8.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
